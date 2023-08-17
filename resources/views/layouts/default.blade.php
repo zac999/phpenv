1efe55ee8fdf210c55028d1149404ad0b5e0819a
phpenv <zhaiixnjian@gmail.com>
2023-08-17 11:00:16 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title','phpenveb')-新手入门</title>
+</head>
+
+<body>
+  @yield('content')
+</body>
+
+</html>
